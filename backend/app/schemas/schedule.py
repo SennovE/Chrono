08454f0301fcf0df8eb7244e979dc80feb7fb871
Egor Sennov <esennov2005@gmail.com>
2025-01-07 18:23:48 +0300

    add validation and better return

--- backend/app/schemas/schedule.py
+++ backend/app/schemas/schedule.py
@@ -1,18 +1,32 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
+from pydantic import BaseModel, Field, model_validator
+from pydantic_core import PydanticCustomError
+from datetime import datetime, timedelta
 from uuid import UUID
 
 
 class ScheduleForm(BaseModel):
     text: str = Field(example="Meeting or task that you need to put to your schedule")
-    start_time: datetime
-    end_time: datetime
-    recurring: bool = Field(description="Should your task be repeated every week?")
+    start_time: datetime = Field(default=datetime.today())
+    end_time: datetime = Field(default=datetime.today() + timedelta(hours=1))
+    recurring: bool = Field(default=False, description="Should your task be repeated every week?")
+
+    @model_validator(mode="after")
+    def validate_date(self):
+        if self.start_time >= self.end_time:
+            raise PydanticCustomError(
+                "time_error",
+                "start_time should be less than end_time",
+                {
+                    "start_time": self.start_time,
+                    "end_time": self.end_time,
+                }
+            )
+        return self
 
 
 class ScheduleResponse(ScheduleForm):
     id: UUID
-    week_day: int
+    week_day: int = Field(default=datetime.today().weekday())
 
 
 class ScheduleUpdateForm(BaseModel):
