b1b871fecd82fe1880d884adca09a3220d3edef3
Denis Galimov <drgalimov@edu.hse.ru>
2025-01-09 20:52:36 +0300

    start update

---
+++ frontend/src/pages/start/components/NavPanel.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
+
+<template>
+    <div class="top-nav">
+        <ul>
+            <li @click="router.push({name: 'Register'})"><b>Регистрация</b></li>
+            <li @click="router.push({name: 'Login Page'})"><b>Авторизация</b></li>
+            <li
+                class="right-button"
+                @click="router.push({name: 'Profile Page'})"
+            ><b>Профиль</b></li>
+        </ul>
+    </div>
+</template>
+
+<style>
+
+
+.top-nav {
+    width: 100%;
+    background: linear-gradient(to left, var(--color-briter-black), var(--color-black));
+    position: fixed;
+    top: 0;
+    left: 0;
+    box-shadow: 0px 0px 20px var(--color-deep-purple);
+    z-index: 999;
+}
+
+.top-nav ul {
+    display: flex;
+    list-style-type: none;
+    justify-content: flex-start;
+}
+
+.top-nav li {
+    display: block;
+    padding: 15px 20px;
+    text-decoration: none;
+    border-radius: 5px;    
+    transition: text-shadow 0.3s ease-in-out;
+    cursor: pointer;
+}
+
+.right-button {
+    margin-left: auto;
+    margin-right: 2%;
+}
+
+</style>
--- frontend/src/pages/start/index.vue
+++ frontend/src/pages/start/index.vue
@@ -1,23 +1,15 @@
+<script setup>
+
+import navPanel from "./components/NavPanel.vue"
+</script>
 <template>
   <div id="app">
-    <nav class="navbar">
-      <ul class="nav-links">
-        <li><router-link to="/register">Регистрация</router-link></li>
-        <li><router-link to="/login">Авторизация</router-link></li>
-        <li><a href="#about">О нас</a></li>
-        <li><a href="#services">Услуги</a></li>
-        <li><a href="#contact">Контакты</a></li>
-      </ul>
-    </nav>
+    <navPanel />
     <h1>Добро пожаловать в Chrono!</h1>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'StartPage',
-};
-</script>
+
 
 <style>
 
