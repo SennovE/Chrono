0c626990ff30b6fcd3aec79372d5dcee9d606c80
Kera <limonchik30ker@gmail.com>
2025-03-16 02:57:38 +0300

    profile some upgrade

--- frontend/src/pages/profile/index.vue
+++ frontend/src/pages/profile/index.vue
@@ -3,42 +3,44 @@ import navPanel from "./components/NavPad.vue";
 import userAvatar from "./components/UserAvatar.vue";
 //import SettingsButton from "./components/SettingsButton.vue";
 import TodayTasks from "./components/TodayTasks.vue";
-//import StatisticData from "./components/StatisticData.vue";
+import StatisticData from "./components/StatisticData.vue";
 //import UserSettings from "./components/UserSettings";
 </script>
 
-
 <template>
-    <div class = "profile-page">
-        <navPanel />
-        <div>
-            <userAvatar />
-        </div>
-        <div>
-            <SettingsButton />
-        </div>
-        <div>
-            <TodayTasks />
-        </div>
-        <div>
-            <StatisticData />
-        </div>
-        <div>
-            <UserSettings />
-        </div>
+  <div class="profile-page">
+    <navPanel />
+    <div>
+      <userAvatar />
     </div>
+    <div>
+      <SettingsButton />
+    </div>
+    <div>
+      <TodayTasks />
+    </div>
+    <div>
+      <StatisticData />
+    </div>
+    <div>
+      <UserSettings />
+    </div>
+  </div>
 </template>
 
 <style>
 .profile-page {
-    overflow-y: hidden;
-    font-family: "Roboto", sans-serif;    
-    background-image: url('./../../assets/finalmain.PNG');
-    height: 100vh;
-    
+  background-color: #1e1e2e; /* Тёмный фон в стиле Mocha */
+  color: #cdd6f4;          /* Светлый текст */
+  font-family: "Roboto", sans-serif;  
+  height: 100vh;
+  overflow-y: auto;
+  padding: 20px;
 }
+
+/* При необходимости можно добавить дополнительные медиа-запросы для адаптивности */
 @media (max-width: 768px) {
-    .profile-page {
+  .profile-page {
     background-size: cover;
     background-position: center;
   }
